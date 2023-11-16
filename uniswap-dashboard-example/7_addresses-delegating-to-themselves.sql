630e1eda826ba59a55be98c72bc6f3ba20421060
Marlene Marz <42071757+JanetMo@users.noreply.github.com>
2023-11-16 15:58:27 +0100

    Update 7_addresses-delegating-to-themselves.sql

--- uniswap-dashboard-example/7_addresses-delegating-to-themselves.sql
+++ uniswap-dashboard-example/7_addresses-delegating-to-themselves.sql
@@ -1,6 +1,6 @@
 -- table calculating the share of addresses that delegated to themselves
 
-SELECT 'delegating to a different address' AS description,
+SELECT 'delegating to the same address' AS description,
 COUNT(evt_tx_hash) AS total_delegation_events,
 COUNT(evt_tx_hash) * 100.00 / (SELECT COUNT(evt_tx_hash) FROM uniswap_ethereum.UNI_evt_DelegateChanged) AS percentage
 FROM uniswap_ethereum.UNI_evt_DelegateChanged
@@ -9,7 +9,7 @@ WHERE delegator = todelegate
 UNION
 
 SELECT 
-'delegating to the same address' AS description,
+'delegating to a different address' AS description,
 COUNT(evt_tx_hash) AS total_delegation_events,
 COUNT(evt_tx_hash) * 100.00 / (SELECT COUNT(evt_tx_hash) FROM uniswap_ethereum.UNI_evt_DelegateChanged) AS percentage
 FROM uniswap_ethereum.UNI_evt_DelegateChanged
