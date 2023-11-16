6c2da9a5a50edd2e169cd46f12f6583d6a058c8a
Marlene Marz <42071757+JanetMo@users.noreply.github.com>
2023-11-16 14:37:07 +0100

    Create 7_addresses-delegating-to-themselves.sql
    new query

---
+++ uniswap-dashboard-example/7_addresses-delegating-to-themselves.sql
@@ -0,0 +1,16 @@
+-- table calculating the share of addresses that delegated to themselves
+
+SELECT 'delegating to a different address' AS description,
+COUNT(evt_tx_hash) AS total_delegation_events,
+COUNT(evt_tx_hash) * 100.00 / (SELECT COUNT(evt_tx_hash) FROM uniswap_ethereum.UNI_evt_DelegateChanged) AS percentage
+FROM uniswap_ethereum.UNI_evt_DelegateChanged
+WHERE delegator = todelegate
+
+UNION
+
+SELECT 
+'delegating to the same address' AS description,
+COUNT(evt_tx_hash) AS total_delegation_events,
+COUNT(evt_tx_hash) * 100.00 / (SELECT COUNT(evt_tx_hash) FROM uniswap_ethereum.UNI_evt_DelegateChanged) AS percentage
+FROM uniswap_ethereum.UNI_evt_DelegateChanged
+WHERE delegator != todelegate
